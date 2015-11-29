484050d2c19012e640bcd4d6cc65f8f48f73b446
David Swift <masterdavidswift@hotmail.com>
2015-11-29 15:52:38 +0000

    fresh start

--- project.clj
+++ project.clj
@@ -4,13 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [com.stuartsierra/component "0.3.0"]
                  [co.paralleluniverse/pulsar "0.7.3"]
-                 [co.paralleluniverse/comsat-httpkit "0.5.0"]
-                 [compojure "1.4.0"]
-                 [org.clojure/clojurescript "1.7.145"]
-                 [org.omcljs/om "1.0.0-alpha14"]
-                 [figwheel-sidecar "0.4.0" :scope "provided"]]
+                 [co.paralleluniverse/comsat-httpkit "0.5.0"]]
   :java-agents [[co.paralleluniverse/quasar-core "0.7.3"]]
   :main ^:skip-aot sms-application.core
   :target-path "target/%s"
--- src/sms_application/core.clj
+++ src/sms_application/core.clj
@@ -1,33 +1,8 @@
 (ns sms-application.core
   (:gen-class)
-  (:require
-    [ring.util.response :refer [file-response]]
-    [ring.middleware.resource :refer [wrap-resource]]
-    [ring.middleware.file-info :refer [wrap-file-info]]
-    [org.httpkit.server :as server]
-    [co.paralleluniverse.fiber.httpkit.client :refer :all]
-    [co.paralleluniverse.pulsar.core :refer [fiber]]
-    [compojure.core :refer :all]
-    [compojure.route :as route]))
+  (:require))
 
-(defn index []
-  (file-response "public/index.html" {:root "resources"}))
 
-(defroutes handler
-  (GET "/" [] (index))
-  (route/resources "/")
-  (route/not-found "not found"))
+(defn -main [])
 
-(defonce server (atom nil))
 
-(defn stop-server []
-  (when-not (nil? @server)
-    (@server :timeout 100)
-    (reset! server nil)))
-
-(defn -main []
-  (reset! server (server/run-server
-                   handler {:port 8080})))
-
-(defn print-message [message]
-  (println (str message)))
