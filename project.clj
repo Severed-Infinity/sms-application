a8b5e58d116affda9e14e87e0dedb71478a78b1c
David Swift <masterdavidswift@hotmail.com>
2015-12-01 12:36:58 +0000

    Made a simple message input process.
    snd and rcv must be wrapped individually in fibers or threads.

--- project.clj
+++ project.clj
@@ -2,11 +2,18 @@
   :description "An sms application designed to use a built in messaging system, web-texts and standard sms functionalities."
   :url "https://github.com/Severed-Infinity/sms-application"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [co.paralleluniverse/pulsar "0.7.3"]
-                 [co.paralleluniverse/comsat-httpkit "0.5.0"]]
-  :java-agents [[co.paralleluniverse/quasar-core "0.7.3"]]
-  :main ^:skip-aot sms-application.core
+                 [co.paralleluniverse/pulsar "0.7.3" :exclusions [org.clojure/tools.analyzer.jvm org.clojure/tools.analyzer]]
+                 #_[co.paralleluniverse/comsat-httpkit "0.5.0"]]
+  :java-agents [[co.paralleluniverse/quasar-core "0.7.3" :options "m"]]
+  :manifest {"Premain-Class"           "co.paralleluniverse.fibers.instrument.JavaAgent"
+             "Agent-Class"             "co.paralleluniverse.fibers.instrument.JavaAgent"
+             "Can-Retransform-Classes" "true"
+             "Can-Redefine-Classes"    "true"}
+  :bootclasspath true
+  :main sms-application.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :auto-instrument-all
+                      {:jvm-opts ["-Dco.paralleluniverse.pulsar.instrument.auto=all"]}})
--- src/sms_application/core.clj
+++ src/sms_application/core.clj
@@ -1,8 +1,24 @@
 (ns sms-application.core
   (:gen-class)
-  (:require))
+  (:use [co.paralleluniverse.pulsar core])
+  (:import [co.paralleluniverse.fibers Fiber])
+  (:refer-clojure :exclude [await promise]))
 
+(def incoming-queue (channel))
 
-(defn -main [])
+(defn incoming-message [src dest message]
+  (fiber
+    (snd incoming-queue [src dest message])))
+
+(def outgoing-queue (channel))
+
+(defn print-message [src dest message]
+  (println src dest message))
+
+(defn -main []
+  (incoming-message "086" "087" "hello")
+  (fiber
+    (let [message (rcv incoming-queue)]
+      (println message))))
 
 
