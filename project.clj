17b122eeec08bcd1ac8edb95dd6f088bb213fb45
David Swift <masterdavidswift@hotmail.com>
2016-01-24 20:21:57 +0000

    added ring and javax serlets for form support
    updated routes to only accept there respective get or post requirements
    
    updated handler to return form data to view
    
    added ring params and multipart-params for form url encoding or request body addition.
    
    todo
    * cookies/token
    * instead of returning just a string with the request do approiate function calls.

--- project.clj
+++ project.clj
@@ -6,7 +6,11 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [co.paralleluniverse/pulsar "0.7.4" :exclusions [org.clojure/tools.analyzer.jvm org.clojure/tools.analyzer]]
                  [co.paralleluniverse/comsat-httpkit "0.5.0"]
-                 [bidi "1.25.0"]]
+                 [bidi "1.25.0"]
+
+                 [ring/ring-core "1.4.0"]
+                 ;requirement for ring multipart
+                 [javax.servlet/servlet-api "2.5"]]
   :java-agents [[co.paralleluniverse/quasar-core "0.7.4" :options "m"]]
   :manifest {"Premain-Class"           "co.paralleluniverse.fibers.instrument.JavaAgent"
              "Agent-Class"             "co.paralleluniverse.fibers.instrument.JavaAgent"
--- src/sms_application/api.clj
+++ src/sms_application/api.clj
@@ -1,23 +1,29 @@
 (ns sms-application.api
   #_(:use [bidi.bidi])
   (:require [bidi.ring :as ring]
-            [bidi.bidi :refer [path-for match-route]]))
+            [bidi.bidi :refer [path-for match-route]]
+            [ring.middleware.params :as params]
+            [ring.middleware.multipart-params :as mul-params]))
 
 ;;possibly remove the spaces for url sake
 ;;with spaces (0|\+353|353)\s{0,1}(83|85|86|87|88|89)\s{0,1}\d{3}\s{0,1}\d{4}
 ;;without spaces (0|\+353|353)(83|85|86|87|88|89)\d{7}
 (def api-routes ["/" {["user/" [#"(0|\+353|353)(83|85|86|87|88|89)\d{7}" :user] "/"]
-                           {"message"  :send-message
-                            "messages" :get-messages}
+                           {"message"  {:post :send-message}
+                            "messages" {:get :get-messages}}
                       true :not-found}])
 
 (def api-handler {:get-messages (fn [req] {:status 200 :body (str "unread messages " (:route-params req))})
-                  :send-message (fn [req] {:status 200 :body (str "sent message " (:route-params req))})
+                  :send-message (fn [req] {:status 200 :body (str "sent message " (:params req))})
                   :not-found    (fn [req] {:status 404 :body (str "not-found {:uri \"" (:uri req) "\"}")})})
 
 (defn key-handler [key] (key api-handler))
 
-(def app (ring/make-handler api-routes key-handler))
+(def app (-> (ring/make-handler api-routes key-handler)
+             ;ring params required to access form data through url&|body
+             params/wrap-params
+             mul-params/wrap-multipart-params)
+  #_(mul-params/wrap-multipart-params (params/wrap-params (ring/make-handler api-routes key-handler))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;testing api route paths
